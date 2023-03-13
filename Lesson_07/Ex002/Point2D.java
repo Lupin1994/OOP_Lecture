6d59a5a73be5e37c361468d68459df418c90c1df
Lupin <skorpion_94@list.ru>
2023-03-13 18:49:59 +0100

    Lecture_1

--- Lesson_07/Ex002/Point2D.java
+++ Lesson_07/Ex002/Point2D.java
@@ -1,6 +1,60 @@
 package Lesson_07.Ex002;
 
+/**
+ * it is point 2D
+ */
 public class Point2D  {
-    int x, y;
-    
+    int x, y; //поле
+
+    /**
+     * This is constacture
+     * @param valueX this is coordinate X
+     * @param valueY this is coordinate Y
+     */
+    public Point2D(int valueX,int valueY ) {
+       x = valueX;
+       y = valueY;
+    }
+
+    public Point2D(int value) {
+        this(value,value);
+    }
+    public Point2D() {
+        this(0);
+    }
+
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+
+    public void setX(int value){
+        this.x = value;
+    }
+    public void setY(int value){
+        this.y = value;
+    }
+
+    // public Point2D() {
+    //     x = 0;
+    //     y = 0;
+    // }
+
+    // public Point2D(int value) {
+    //     x = value;
+    //     y = value;
+    // }
+    private String getInfo(){
+        return String.format("x: %d; y: %d", x,y);
+    }
+    @Override
+    public String toString() {
+
+        return getInfo();
+    }
+
+    public static void distance(Point2D a, Point2D b) {
+    }
 }
--- Lesson_07/Ex002/Program.java
+++ Lesson_07/Ex002/Program.java
@@ -10,16 +10,18 @@ public class Program {
     }
 
     public static void main(String[] args) {
-        Point2D a = new Point2D();
-        a.x = 0;
-        a.y = 2;
-        System.out.println(a.toString());
+        Point2D a = new Point2D(0,2); // экземпляр класса
+        a.setX(12);
+        System.out.println(a);
 
-        Point2D b = new Point2D();
-        b.x = 0;
-        b.y = 10;
-        System.out.println(b.toString());
+        System.out.println(a.getX());
+        System.out.println(a.getY());    
+
+        Point2D b = new Point2D(10); // экземпляр класса
+
+        System.out.println(b.toString()); // переориенированный toString
         System.out.println(distance(a, b));
-        
+
+
     }
 }
